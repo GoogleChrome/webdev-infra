6d50638f417c8be75a7297f8c5c8774dc44d3075
Michael Solati <mkslt04@gmail.com>
2021-07-28 20:28:04 -0700

    docs(video): fix typo

--- types/shortcodes/Video.d.ts
+++ types/shortcodes/Video.d.ts
@@ -53,7 +53,7 @@ export type VideoArgs = {
   linkTo?: boolean;
   /**
    * A Boolean attribute that indicates the default setting of the audio contained in the video.
-   * Defgaults to true if `autoplay` is enabled.
+   * Defaults to true if `autoplay` is enabled because browsers require autoplay videos to also be muted.
    */
   muted?: boolean;
   /**
