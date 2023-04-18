50f783f5a6cdfc0f3ef39f7a401c2c895947b7e5
Maya Janthong <maya@tokyo.uk>
2023-04-18 22:16:58 +0700

    feat: normalised the feeds and complete the methods for fetching XML feeds

--- utils/rss-feeds.js
+++ utils/rss-feeds.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Google LLC
+ * Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,23 @@
  * limitations under the License.
  */
 
+/**
+ * @fileoverview A single method that grabs the title, permalink, date,
+ * and content from the arbitrary XML feeds, normalised and 
+ * write a JSON file, containing the feed data needed for the updated author page,
+ * to the src/site/_data/ path for both d.c.c and web.dev.
+ */
+
 const fs = require('fs');
 const jsdom = require('jsdom');
 const fetch = require('node-fetch');
-const authorsData = fs.readFileSync('src/site/_data/authorsData.json', 'utf8');
 
+/**
+ * Grab the content from the target HTML tag
+ * @param {HTMLElement} parent the HTMLElement of the article.
+ * @param {string} target A name of the target HTML tag in the article.
+ * @return {string} The content in the target HTML tag.
+ */
 const getElementsByTag = (parent, target) => {
   const collections = parent.getElementsByTagName(target);
 
@@ -28,11 +40,37 @@ const getElementsByTag = (parent, target) => {
 }
 
 /**
+ * Sort the feeds from all sources by date from newest to the oldest.
+ * @param {Array} feeds A collection of the articles.
+ * @return {Array} A collection of the articles, which are sorted by date.
+ */
+const sortFeeds = (feeds) => {
+  return feeds.sort((a, b) => {
+    const d1 = new Date(a.date);
+    const d2 = new Date(b.date);
+    
+    return d2.valueOf() - d1.valueOf();
+  });
+}
+
+/**
+ * Grab the title, permalink, date, and content from the XML data.
+ * @param {HTMLElement} entry A XML path.
+ * @return {string} The permalink
+ */
+const getPermalink = (entry) => {
+  const guid = getElementsByTag(entry, 'guid');
+  const href = entry.getElementsByTagName('link')[0].getAttribute('href');
+
+  return guid || href || '';
+}
+
+/**
  * Grab the title, permalink, date, and content from the XML data.
  * @param {string} xmlPath A XML path.
  * @return {Promise<object>} A list of feeds after extract the XML.
  */
-const extractionXML = async (xmlPath) => {
+const extractXML = async (xmlPath) => {
   const response = await fetch(xmlPath);
   const body = await response.text();
   const dom = await new jsdom.JSDOM(body);
@@ -49,20 +87,22 @@ const extractionXML = async (xmlPath) => {
     const postData = { source: xmlPath };
 
     postData.title = getElementsByTag(entry, 'title');
-    postData.date = getElementsByTag(entry, 'updated');
     postData.summary = getElementsByTag(entry, 'summary');
-    postData.url = entry.getElementsByTagName('link')[0].getAttribute('href');
+    postData.date = getElementsByTag(entry, 'updated');
 
+    postData.url = getPermalink(entry);
+  
     feeds.push(postData);
   };
-    
+
   for (const item of itemTagCollection) {
     const postData = { source: xmlPath };
 
     postData.title = getElementsByTag(item, 'title');
-    postData.date = getElementsByTag(item, 'pubDate');
     postData.summary = getElementsByTag(item, 'description');
-    postData.url = item.querySelectorAll('link')[0].innerHTML; // TO DO: clean up link tag first
+    postData.date = getElementsByTag(item, 'pubDate');
+
+    postData.url = getPermalink(item);
 
     feeds.push(postData);
   };
@@ -70,8 +110,9 @@ const extractionXML = async (xmlPath) => {
   return feeds;
 }
 
-const externalPosts = async () => {
+const externalPosts = async (authorsData) => {
   const authorData = JSON.parse(authorsData);
+  const sortedFeeds = [];
 
   for (const author in authorData) {
     const externalPath = authorData[author].external;
@@ -80,9 +121,11 @@ const externalPosts = async () => {
     if (!externalPath?.length) break;
 
     for (const path of externalPath) {
-      const feeds = await extractionXML(path.url);
-      authorData[author].feeds = [...authorData[author].feeds, ...feeds];
+      const feeds = await extractXML(path.url);
+      sortedFeeds.push(...feeds);
     }
+
+    authorData[author].feeds = sortFeeds(sortedFeeds);
   };
 
   fs.writeFileSync(
